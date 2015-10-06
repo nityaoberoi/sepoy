6db3a243b50b6b639f09d1f7bf1fa75f8391b8dd
Sriram Raman <sriram@airtime.com>
2015-10-05 23:18:10 -0400

    Add new images

--- static/js/sepoy-gallery.js
+++ static/js/sepoy-gallery.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
     var images = [
+        "/static/img/gallery/images/sepoy_digital_small_Car_sitting.jpg",
+        "/static/img/gallery/images/sepoy_digital_small_Goth.jpg",
+        "/static/img/gallery/images/sepoy_digital_small_MG_Car_Standing.jpg",
+        "/static/img/gallery/images/sepoy_digital_small_MG_Hand.jpg",
+        "/static/img/gallery/images/sepoy_digital_small_MG_Side_Jacket.jpg",
+        "/static/img/gallery/images/sepoy_digital_small_MG_Wall_Sleeping.jpg",
+        "/static/img/gallery/images/sepoy_digital_small_Rock_Standing.jpg",
+        "/static/img/gallery/images/sepoy_digital_small_Sit_Down.jpg",
+        "/static/img/gallery/images/sepoy_digital_small_Straight_jacket.jpg",
+        "/static/img/gallery/images/sepoy_film_small03_Rock_sitting.jpg",
         "/static/img/gallery/images/ameylighter.jpg",
         "/static/img/gallery/images/anupamkeys.jpg",
         "/static/img/gallery/images/rata.jpg",
         "/static/img/gallery/images/all_fontanas.jpg",
         "/static/img/gallery/images/sepoytrashbar.jpg",
         "/static/img/gallery/images/amey_trashbar.jpg",
-        "/static/img/gallery/images/rata_pam_trashbar.jpg",
         "/static/img/gallery/images/all_paperbox.jpg",
         "/static/img/gallery/images/anupam_paperbox.jpg"
     ];
